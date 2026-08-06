e48ea0c6424532d1730dc3ff3ebb779fc1a21dec
kaduvill <jottafanftw@gmail.com>
2026-08-06 21:22:49 +0200

    Fixed MODULARFULLSCREEN using raw framebuffer dimensions instead of Minecraft scaled GUI dimensions. This corrects clipping, mouse input, and module positioning across GUI scales.

--- src/main/java/zmaster587/libVulpes/inventory/GuiModularFullScreen.java
+++ src/main/java/zmaster587/libVulpes/inventory/GuiModularFullScreen.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import zmaster587.libVulpes.inventory.modules.IModularInventory;
 import zmaster587.libVulpes.inventory.modules.ModuleBase;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import yalter.mousetweaks.api.MouseTweaksDisableWheelTweak;
 
@@ -15,20 +14,12 @@ public class GuiModularFullScreen extends GuiModular {
 			List<ModuleBase> modules, IModularInventory modularInv,
 			boolean includePlayerInv, boolean includeHotBar, String name) {
 		super(playerInv, modules, modularInv, includePlayerInv,includeHotBar, name);
-		
-		this.xSize = Minecraft.getMinecraft().displayWidth;
-		this.ySize = Minecraft.getMinecraft().displayHeight;
-		this.width = Minecraft.getMinecraft().displayWidth;
-		this.height = Minecraft.getMinecraft().displayHeight;
 	}
-	
+
 	@Override
 	public void initGui() {
-		this.xSize = Minecraft.getMinecraft().displayWidth;
-		this.ySize = Minecraft.getMinecraft().displayHeight;
-		this.width = Minecraft.getMinecraft().displayWidth;
-		this.height = Minecraft.getMinecraft().displayHeight;
-		
+		this.xSize = this.width;
+		this.ySize = this.height;
 		super.initGui();
 	}
 }
